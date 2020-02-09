19985fc4a93ff3cec37aac5ab20ee05754252adb
Nguyễn Đắc Hiếu <dachieu1996@gmail.com>
2020-02-09 22:57:16 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4wTTu3GqY0n5jCwR6NGE1BpW_WgYdeZs",
+    authDomain: "oshop-51eed.firebaseapp.com",
+    databaseURL: "https://oshop-51eed.firebaseio.com",
+    projectId: "oshop-51eed",
+    storageBucket: "oshop-51eed.appspot.com",
+    messagingSenderId: "639791586184",
+    appId: "1:639791586184:web:f696e4cb14dedcae14383d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4wTTu3GqY0n5jCwR6NGE1BpW_WgYdeZs",
+    authDomain: "oshop-51eed.firebaseapp.com",
+    databaseURL: "https://oshop-51eed.firebaseio.com",
+    projectId: "oshop-51eed",
+    storageBucket: "oshop-51eed.appspot.com",
+    messagingSenderId: "639791586184",
+    appId: "1:639791586184:web:f696e4cb14dedcae14383d"
+  }
 };
 
 /*
